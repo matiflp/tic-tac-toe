f946022460cf2002f9bdf94526bb715b1ee29697
Matias Francisco Lopez Artes <matias.lopez@raona.com>
2022-05-29 16:41:41 -0300

    Se añadieron las dos siguiente funcionalidades: Cuando alguien gana, resalta los 3 cuadrados que hicieron que gane y cuando nadie gana, muestra un mensaje acerca de que el resultado es un empate.

--- src/index.js
+++ src/index.js
@@ -3,18 +3,27 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 
 function Square(props) {
+
+  var isWinner = false;
+  props.posWins.forEach(item => {
+  if((props.index === item) && item != null )
+    isWinner = true;
+  });
+
   return (
-    <button className="square" onClick={props.onClick}>
+    <button className = {isWinner ? 'win' : 'square'} onClick={props.onClick}>
       {props.value}
     </button>
   );
 }
 
 class Board extends React.Component {
-  renderSquare(i) {
+  renderSquare(i) {    
     return (
       <Square
         key = {i}
+        index = {i}
+        posWins = {this.props.posWins}
         value={this.props.squares[i]}
         onClick={() => this.props.onClick(i)}
       />
@@ -48,15 +57,21 @@ class Game extends React.Component {
         clickPosition: 0
       }],
       stepNumber: 0,
-      xIsNext: true
+      xIsNext: true,
+      isAscending: true,
+      squaresWins: Array(3).fill(null),
+      countSquares: 0
     };
+    this.handleClick = this.handleClick.bind(this);
   }
 
   handleClick(i) {
     const history = this.state.history.slice(0, this.state.stepNumber + 1);
     const current = history[history.length - 1];
     const squares = current.squares.slice();
-    if (calculateWinner(squares) || squares[i]) {
+    const [winner, , ,] = calculateWinner(squares);
+    
+    if (winner || squares[i]) {
       return;
     }
     squares[i] = this.state.xIsNext ? 'X' : 'O';
@@ -67,6 +82,17 @@ class Game extends React.Component {
       }]),
       stepNumber: history.length,
       xIsNext: !this.state.xIsNext,
+      countSquares: this.state.countSquares+1
+    }, () => {
+      const history = this.state.history.slice(0, this.state.stepNumber + 1);
+      const current = history[history.length - 1];
+      const squares = current.squares.slice();
+      const [winner, pos1, pos2, pos3] = calculateWinner(squares);
+      if(winner){
+        this.setState({
+          squaresWins: [pos1, pos2, pos3]
+        })
+      }
     });
   }
 
@@ -77,10 +103,16 @@ class Game extends React.Component {
     });
   }
 
+  orderToggle(){
+    this.setState({
+      isAscending: !this.state.isAscending
+    });
+  }
+
   render() {
     const history = this.state.history;
     const current = history[this.state.stepNumber];
-    const winner = calculateWinner(current.squares);
+    const [winner,,,] = calculateWinner(current.squares);
 
     const moves = history.map((step, move) => {
       let col = 0;
@@ -116,9 +148,16 @@ class Game extends React.Component {
       );
     });
 
+    const isAscending = this.state.isAscending;
+    if(!isAscending){
+      moves.reverse();
+    }
+
     let status;
     if (winner) {
       status = 'Winner: ' + winner;
+    } else if(this.state.countSquares === 9) {
+      status = 'The result is a Tie';
     } else {
       status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
     }
@@ -128,12 +167,15 @@ class Game extends React.Component {
         <div className="game-board">
           <Board
             squares={current.squares}
+            posWins = {this.state.squaresWins}
             onClick={(i) => this.handleClick(i)}
           />
         </div>
+        
         <div className="game-info">
           <div>{status}</div>
-          <ol>{moves}</ol>
+          <button onClick={() => this.orderToggle()}>{isAscending ? 'Ascending' : 'Descending'}</button>
+          <ol>{moves}</ol> 
         </div>
       </div>
     );
@@ -159,8 +201,8 @@ function calculateWinner(squares) {
   for (let i = 0; i < lines.length; i++) {
     const [a, b, c] = lines[i];
     if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-      return squares[a];
+      return [squares[a], a, b, c];
     }
   }
-  return null;
+  return [null, null, null, null];
 }
